3fedfedcb9544edde86d7d9d24df81922e72ce92
Poowadol Rungtabnapa <51953419+PoowadolRungtabnapa@users.noreply.github.com>
2020-10-21 15:48:34 +0700

    .

--- main.py
+++ main.py
@@ -136,14 +136,12 @@ def admin() :
                     if j.Type < 4 :
                         if j.Type == 1 :
                             Type = "In Queue"
-                            Value = {'Name' : i.name, "ID Order" : j.id, "Date": j.date, "Type" : Type}
                         elif j.Type == 2 :
                             Type = "On Cooking"
-                            Value = {'Name' : i.name, "ID Order" : j.id, "Date": j.date, "Type" : Type}
                         elif j.Type == 3 :
                             Type = "On Delivery"
-                            Value = {'Name' : i.name, "ID Order" : j.id, "Date": j.date, "Type" : Type}
-                        Username.append(Value)
+                    Value = {'Name' : i.name, "ID Order" : j.id, "Date": j.date, "Type" : Type}
+                    Username.append(Value)
             if request.method == 'POST' : # ตรวจสอบค่าที่ได้รับมา
                 if request.form.get("Type") : 
                     O = db.session.query(Order).filter_by(id=request.form.get('ID')).first()
